d70ed0a53d07387b736bda13964d6912fa893ac2
Valdis Indricans <vav4ik@pop-os.localdomain>
2023-11-23 19:27:32 +0000

    Logout and protect auth resource in DB

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
@@ -5,9 +6,9 @@ import { StatusBar } from "expo-status-bar";
 import LoginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/SignupScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
+import IconButton from "./components/ui/IconButton";
 import { Colors } from "./constants/styles";
 import AuthContentProvider, { AuthContext } from "./store/auth-context";
-import { useContext } from "react";
 
 const Stack = createNativeStackNavigator();
 
@@ -27,6 +28,9 @@ function AuthStack() {
 }
 
 function AuthenticatedStack() {
+  
+  const authCtx = useContext(AuthContext);
+
   return (
     <Stack.Navigator
       screenOptions={{
@@ -35,7 +39,20 @@ function AuthenticatedStack() {
         contentStyle: { backgroundColor: Colors.primary100 },
       }}
     >
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        options={{
+          headerRight: ({ tintColor }) => (
+            <IconButton
+              icon="exit"
+              color={tintColor}
+              size={24}
+              onPress={authCtx.logout}
+            />
+          ),
+        }}
+      />
     </Stack.Navigator>
   );
 }
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useContext, useEffect, useState } from "react";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import axios from "axios";
+
+import { AuthContext } from "../store/auth-context";
 
 function WelcomeScreen() {
+  const authCtx = useContext(AuthContext);
+
+  const [fetchedMessage, setFetchedMessage] = useState("");
+
+  const token = authCtx.token;
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://shwarzmuller-react-http-default-rtdb.europe-west1.firebasedatabase.app/message.json?auth=${token}`
+      )
+      .then((response) => {
+        setFetchedMessage(response.data);
+      })
+      .catch((error) => Alert.alert("Can't fetch private data", error.message));
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
@@ -14,13 +36,13 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 32,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
   },
 });
