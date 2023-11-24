72507c6dba9d81ceea6d08bcda4bb5db79828f67
Valdis Indricans <vav4ik@pop-os.localdomain>
2023-11-24 19:25:02 +0000

    Store token on the devise, prolong splash screen till appIsReady

--- App.js
+++ App.js
@@ -1,23 +1,29 @@
-import { useContext } from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StatusBar } from "expo-status-bar";
-
-import LoginScreen from "./screens/LoginScreen";
-import SignupScreen from "./screens/SignupScreen";
-import WelcomeScreen from "./screens/WelcomeScreen";
-import IconButton from "./components/ui/IconButton";
-import { Colors } from "./constants/styles";
-import AuthContentProvider, { AuthContext } from "./store/auth-context";
-
+import { useContext, useEffect, useState, useCallback } from 'react';
+import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as SplashScreen from 'expo-splash-screen';
+ 
+import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import { Colors } from './constants/styles';
+import AuthContextProvider, { AuthContext } from './store/auth-context';
+import IconButton from './components/ui/IconButton';
+ 
 const Stack = createNativeStackNavigator();
 
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+ 
 function AuthStack() {
   return (
     <Stack.Navigator
       screenOptions={{
         headerStyle: { backgroundColor: Colors.primary500 },
-        headerTintColor: "white",
+        headerTintColor: 'white',
         contentStyle: { backgroundColor: Colors.primary100 },
       }}
     >
@@ -26,16 +32,14 @@ function AuthStack() {
     </Stack.Navigator>
   );
 }
-
+ 
 function AuthenticatedStack() {
-  
   const authCtx = useContext(AuthContext);
-
   return (
     <Stack.Navigator
       screenOptions={{
         headerStyle: { backgroundColor: Colors.primary500 },
-        headerTintColor: "white",
+        headerTintColor: 'white',
         contentStyle: { backgroundColor: Colors.primary100 },
       }}
     >
@@ -56,24 +60,58 @@ function AuthenticatedStack() {
     </Stack.Navigator>
   );
 }
-
+ 
 function Navigation() {
   const authCtx = useContext(AuthContext);
-
   return (
     <NavigationContainer>
-      {authCtx.isAuthenticated ? <AuthenticatedStack /> : <AuthStack />}
+      {!authCtx.isAuthenticated && <AuthStack />}
+      {authCtx.isAuthenticated && <AuthenticatedStack />}
     </NavigationContainer>
   );
 }
-
+ 
+function Root() {
+  const [appIsReady, setAppIsReady] = useState(false);
+  const authCtx = useContext(AuthContext);
+ 
+  useEffect(() => {
+    const fetchToken = async () => {
+      const storedToken = await AsyncStorage.getItem('token');
+ 
+      if (storedToken) {
+        authCtx.authenticate(storedToken);
+      }
+ 
+      setAppIsReady(true);
+    };
+    fetchToken();
+  }, []);
+ 
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+ 
+  if (!appIsReady) {
+    return null;
+  }
+ 
+  return (
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <Navigation />
+    </View>
+  );
+}
+ 
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <AuthContentProvider>
-        <Navigation />
-      </AuthContentProvider>
+      <AuthContextProvider>
+        <Root />
+      </AuthContextProvider>
     </>
   );
 }
